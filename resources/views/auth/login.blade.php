26a958dd959f8bae3bb94850db4b58a221387c4a
Aidil <aidil03f@gmail.com>
2021-04-22 03:23:46 +0800

    setup layouts

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.auth',['title' => 'Login'])
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+               Lorem ipsum dolor sit olorem ex error! At?
+        </div>
+    </div>
+@endsection
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.auth',['title' => 'Register'])
+
+@section('content')
+    <div class="card">
+        <div class="card-header">Register</div>
+        <div class="card-body">
+               Lorem ipsum dolor sit olorem ex error! At?
+        </div>
+    </div>
+@endsection

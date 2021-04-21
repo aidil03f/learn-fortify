84e7f1a6e072405504d03a30fcd40037f5196958
Aidil <aidil03f@gmail.com>
2021-04-22 04:10:10 +0800

    register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,40 @@
     <div class="card">
         <div class="card-header">Register</div>
         <div class="card-body">
-               Lorem ipsum dolor sit olorem ex error! At?
+            <form action="/register" method="post">
+            @csrf
+         
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror">
+                    @error('name')
+                        <span class="invalid-feedback">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" class="form-control @error('email') is-invalid @enderror">
+                    @error('email')
+                        <span class="invalid-feedback">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
+                    @error('password')
+                        <span class="invalid-feedback">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">Confirm Password</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                </div>
+
+                <button type="submit" class="btn btn-block btn-primary">Register</button>
+            </form>
         </div>
     </div>
 @endsection
